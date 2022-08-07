6aefa78672b36fb27abb962d6369bdb44331203f
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2022-08-07 14:55:15 +0300

    update workflow

--- archiveAll.ts
+++
@@ -1,9 +0,0 @@
-import { archiveBundle } from "./archiveBundle";
-import { archiveSource } from "./archiveSource";
-
-async function archiveAll() {
-  await archiveSource();
-  await archiveBundle();
-}
-
-archiveAll();
